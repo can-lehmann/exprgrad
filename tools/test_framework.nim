3cd5e0533d8908b65121e13f75fa73867e36c591
jiro <jiroron666@gmail.com>
2021-11-17 06:14:28 +0900

    Add CI tests (#1)
    * Change directory name
    
    * Add docker environment
    
    * Add test task
    
    * Add CI tests
    
    * Revert "Add docker environment"
    
    This reverts commit 770e4e752d8b6fca4f8679b0917aa45df1e5657c.
    
    * Revert "Add test task"
    
    This reverts commit cc4dde2df7cbc11e6340e8970397b325566a1d0a.
    
    * Delete docker test
    
    * Process returns 1 when tests were failed
    
    * camelCase to snake_case

--- tools/test_framework.nim
+++ tools/test_framework.nim
@@ -14,7 +14,12 @@
 
 # A simple unit-testing framework
 
-import std/[terminal, macros, sets]
+import std/[terminal, macros, sets, exitprocs]
+
+var quit_code = QuitSuccess
+add_exit_proc(proc() {.closure.} =
+  quit(quit_code)
+)
 
 type TestError = ref object of CatchableError
   env: seq[(string, string)]
@@ -28,7 +33,7 @@ template test*(name: string, body: untyped) =
   except TestError as err:
     success = false
     env = err.env
-  
+
   if success:
     stdout.set_foreground_color(fgGreen)
     stdout.write("[✓] ")
@@ -36,6 +41,7 @@ template test*(name: string, body: untyped) =
     stdout.write(name)
     stdout.write("\n")
   else:
+    quit_code = QuitFailure
     stdout.write("\n")
     stdout.set_foreground_color(fgRed)
     stdout.write("Test Failed: ")
