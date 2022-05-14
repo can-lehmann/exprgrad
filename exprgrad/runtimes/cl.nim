ae63e4cb933611dc2cce8f4711e07a435ddaa5c6
Can Lehmann <can.l@posteo.de>
2022-05-14 19:21:52 +0200

    Implement OpenCL runtime

--- exprgrad/runtimes/cl.nim
+++ exprgrad/runtimes/cl.nim
@@ -12,15 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import opencl
+import opencl except check
 
 type
+  GpuError* = ref object of CatchableError
+  
   GpuDevice* = object
     id: PDeviceId
   
   GpuContext* = ref object
+    device: GpuDevice
     context: PContext
     commands: PCommandQueue
+  
+  GpuBuffer* = object
+    ctx: GpuContext
+    size: int
+    mem: PMem
+  
+  GpuKernel* = ref object
+    ctx: GpuContext
+    kernel: PKernel
+
+proc check(res: TClResult) =
+  if res != SUCCESS:
+    raise GpuError(msg: $res)
 
 proc list_platforms(): seq[PPlatformId] =
   var platform_count: uint32
@@ -61,10 +77,91 @@ proc is_gpu*(device: GpuDevice): bool =
   result = (device_type.int64 and DEVICE_TYPE_GPU.int64) != 0
 
 proc new_gpu_context*(device: GpuDevice): GpuContext =
-  discard
+  result = GpuContext(device: device)
+  
+  var
+    id = device.id
+    status: TClResult
+  result.context = create_context(nil, 1, id.addr, nil, nil, status.addr)
+  check status
+  
+  result.commands = create_command_queue(result.context, id, 0, status.addr)
+  check status
 
 proc new_gpu_context*(): GpuContext =
-  result = new_gpu_context(list_devices()[0])
+  let devices = list_devices()
+  if devices.len == 0:
+    raise GpuError(msg: "Unable to find device")
+  result = new_gpu_context(devices[0])
+
+proc alloc_buffer*(ctx: GpuContext, size: int): GpuBuffer =
+  var status: TClResult
+  result.ctx = ctx
+  result.size = size
+  result.mem = create_buffer(ctx.context, MEM_READ_WRITE, size, nil, status.addr)
+  check status
+
+proc write*(buffer: GpuBuffer, data: pointer, size: int) =
+  if size != buffer.size:
+    raise GpuError(msg: "Attempted to write " & $size & " bytes, but the size of the buffer is " & $buffer.size & " bytes")
+  check buffer.ctx.commands.enqueue_write_buffer(
+    buffer.mem, CL_TRUE, 0, size, data, 0, nil, nil
+  )
+
+proc write*[T](buffer: GpuBuffer, data: openArray[T]) =
+  if data.len > 0:
+    buffer.write(data[0].unsafe_addr, sizeof(T) * data.len)
+
+proc read*[T](buffer: GpuBuffer): seq[T] =
+  if buffer.size mod sizeof(T) != 0:
+    raise GpuError(msg: "Buffer size is not divisible by item type size")
+  if buffer.size > 0:
+    result = new_seq[T](buffer.size div sizeof(T))
+    check buffer.ctx.commands.enqueue_read_buffer(
+      buffer.mem, CL_TRUE, 0, buffer.size, result[0].addr, 0, nil, nil
+    )
+
+proc compile*(ctx: GpuContext, name, source: string): GpuKernel =
+  result = GpuKernel(ctx: ctx)
+  
+  let strings = alloc_cstring_array([source])
+  defer: dealloc_cstring_array(strings)
+  
+  var
+    length = source.len
+    status: TClResult
+  let program = ctx.context.create_program_with_source(1, strings, length.addr, status.addr)
+  check status
+  
+  status = build_program(program, 1, ctx.device.id.addr, nil, nil, nil)
+  
+  if status == BUILD_PROGRAM_FAILURE:
+    var log_length: int
+    check get_program_build_info(program, ctx.device.id, PROGRAM_BUILD_LOG, 0, nil, log_length.addr)
+    if log_length > 0:
+      var log = new_string(log_length)
+      check get_program_build_info(program, ctx.device.id, PROGRAM_BUILD_LOG, log_length, log[0].addr, nil)
+      raise GpuError(msg: "Failed to build program: " & log)
+    else:
+      raise GpuError(msg: "Failed to build program")
+  else:
+    check status
+  
+  result.kernel = create_kernel(program, name.cstring, status.addr)
+  check status
+
+proc run*(kernel: GpuKernel, buffers: openArray[(int, GpuBuffer)], global_size, local_size: openArray[int]) =
+  for (id, buffer) in buffers:
+    check set_kernel_arg(kernel.kernel, uint32(id), sizeof(Pmem), buffer.mem.unsafe_addr)
+  check enqueue_nd_range_kernel(
+    kernel.ctx.commands,
+    kernel.kernel,
+    uint32(global_size.len),
+    nil,
+    global_size[0].unsafe_addr,
+    local_size[0].unsafe_addr,
+    0, nil, nil
+  )
 
 when is_main_module:
   for device in list_devices():
@@ -72,3 +169,30 @@ when is_main_module:
     echo "Vendor: ", device.vendor
     echo "OpenCL Version: ", device.version
     echo "Is GPU: ", device.is_gpu
+  
+  let source = """
+    __kernel void add(__global float* a, __global float* b, __global float* c) {
+      int index = get_global_id(0);
+      c[index] = a[index] + b[index];
+    }
+  """
+  
+  let ctx = new_gpu_context()
+  
+  let
+    a = ctx.alloc_buffer(sizeof(float32) * 32)
+    b = ctx.alloc_buffer(sizeof(float32) * 32)
+    c = ctx.alloc_buffer(sizeof(float32) * 32)
+    data = block:
+      var data = new_seq[float32](32)
+      for it, item in data.mpairs:
+        item = float32(it)
+      data
+  a.write(data)
+  b.write(data)
+  
+  let kernel = ctx.compile("add", source)
+  kernel.run({0: a, 1: b, 2: c}, [32], [32])
+  
+  echo read[float32](c)
+  
